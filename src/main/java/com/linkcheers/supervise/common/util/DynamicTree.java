a8a5ff8cdaef403071448b9a7f2e35737a70febb
毕小康 <1187886093@qq.com>
2020-04-22 16:58:28 +0800

    树优化

--- src/main/java/com/linkcheers/supervise/common/util/DynamicTree.java
+++ src/main/java/com/linkcheers/supervise/common/util/DynamicTree.java
@@ -11,7 +11,8 @@ import java.util.stream.Collectors;
 public class DynamicTree {
 	/**
 	 * 使用递归方法建树
-	 *
+	 * 使用时如果传入对象的子节点名称不是pid需要在对象中覆盖
+	 * 栗子:	 parentId为t对象的父类节点名称，可以在构造容器中super.pid = parentId;
 	 * @param e
 	 * @param p
 	 * @return
@@ -28,7 +29,7 @@ public class DynamicTree {
 
 	/**
 	 * 递归查找子节点
-	 *
+	 * 不带条件的
 	 * @param e
 	 * @param n
 	 * @return
@@ -44,10 +45,45 @@ public class DynamicTree {
 		});
 		return e;
 	}
-
 	/**
-	 * lambda
-	 *
+	 * 使用递归方法建树
+	 * 需要对某些节点剔除，使用时如果传入对象的子节点名称不是pid需要在对象中覆盖
+	 * 栗子:	 parentId为t对象的父类节点名称，可以在构造容器中super.pid = parentId;
+	 * 并且需要要对t 对象根据业务对isPermit值进行覆盖
+	 * @param e
+	 * @param p
+	 * @return
+	 */
+	public static <E extends BaseTree<E>> List<E> buildTreeP(List<E> e, String p) {
+		List<E> t = new ArrayList<>();
+		e.forEach(a -> {
+			if (p.equals(a.getPid())&&a.isPermit()) {
+				t.add(findChildP(a, e));
+			}
+		});
+		return t;
+	}
+	/**
+	 * 递归查找子节点
+	 * 带条件的
+	 * @param e
+	 * @param n
+	 * @return
+	 */
+	public static <E extends BaseTree<E>> E findChildP(E e, List<E> n) {
+		n.forEach(a -> {
+			if (e.getId().equals(a.getPid())&&a.isPermit()) {
+				if (e.getChild() == null) {
+					e.setChild(new ArrayList<>());
+				}
+				e.getChild().add(findChildP(a, n));
+			}
+		});
+		return e;
+	}
+	/**
+	 * 遍历树，使用时如果传入对象的子节点名称不是pid需要在对象中覆盖
+	 * 栗子:	 parentId为t对象的父类节点名称，可以在构造容器中super.pid = parentId;
 	 * @param t
 	 * @param p
 	 * @return
@@ -67,6 +103,36 @@ public class DynamicTree {
 							a.setChild(new ArrayList<>());
 						}
 						a.getChild().add(y);
+					}
+			);
+		});
+		return c;
+	}
+
+	/**
+	 * 需要对某些节点剔除，使用时如果传入对象的子节点名称不是pid需要在对象中覆盖
+	 * 栗子:	 parentId为t对象的父类节点名称，可以在构造容器中super.pid = parentId;
+	 * 并且需要要对t 对象根据业务对isPermit值进行覆盖
+	 * @param t
+	 * @param p
+	 * @return
+	 */
+	@Deprecated
+	public static <E extends BaseTree<E>> List<E> bulidTreed(List<E> t, String p) {
+		List<E> c = t.stream().filter(a -> p.equals(a.getPid())&& a.isPermit()
+		).collect(Collectors.toList());
+		List<E> e = t.stream().filter(
+				a -> !p.equals(a.getPid())
+		).collect(Collectors.toList());
+		c.forEach(a -> {
+			bulidTree(e, a.getId()).forEach(
+					y -> {
+						if (a.getChild() == null) {
+							a.setChild(new ArrayList<>());
+						}
+						if(y.isPermit()){
+							a.getChild().add(y);
+						}
 					}
 			);
 		});
--- src/main/java/com/linkcheers/supervise/dto/BaseTree.java
+++ src/main/java/com/linkcheers/supervise/dto/BaseTree.java
@@ -1,5 +1,6 @@
 package com.linkcheers.supervise.dto;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
@@ -10,13 +11,59 @@ import java.util.List;
  * @author Soya
  */
 @MappedSuperclass
-@Data
 public abstract class BaseTree<T> extends BaseDto<T> {
 	@JsonIgnore
+	@TableField(exist = false)
 	protected String id;
 	@JsonIgnore
+	@TableField(exist = false)
 	protected String pid;
 	@JsonIgnore
+	@TableField(exist = false)
+	protected boolean permit;
+	@JsonIgnore
+	@TableField(exist = false)
 	protected T t;
+	@TableField(exist = false)
 	protected List<T> child;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public boolean isPermit() {
+		return permit;
+	}
+
+	public void setPermit(boolean permit) {
+		this.permit = permit;
+	}
+
+	public T getT() {
+		return t;
+	}
+
+	public void setT(T t) {
+		this.t = t;
+	}
+
+	public List<T> getChild() {
+		return child;
+	}
+
+	public void setChild(List<T> child) {
+		this.child = child;
+	}
 }
